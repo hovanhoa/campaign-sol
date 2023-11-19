f5b42b2377201bbcf70a0890a563e5db031b1907
hovanhoa <hovanhoaa1@gmail.com>
2023-11-20 00:16:37 +0700

    test: contributor money

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -40,4 +40,14 @@ describe('Campaigns', () => {
       const manager = await campaign.methods.manager().call();
       assert.equal(manager, accounts[0]);
     });
+
+    it('allows people to contribute money and marks them as approvers', async () => {
+       await campaign.methods.contribute().send({
+           value: '200',
+           from: accounts[1]
+        });
+
+       const isContributor = await  campaign.methods.approvers(accounts[1]).call();
+       assert.ok(isContributor);
+    });
 });
